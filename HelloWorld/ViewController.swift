ccfe09335c941e97da4293dc910cb03055d73eaa
alexpios <kuchinsky213@gmail.com>
2023-07-22 15:56:08 +0300

    add button + methods

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet var myAppLabel: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myAppLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
     }
-
-
+    @IBAction func onClick() {
+        
+        if myAppLabel.isHidden{
+            myAppLabel.isHidden = false
+            toggleButton.setTitle("Hide text", for: .normal)
+        } else{
+            toggleButton.setTitle("Show text", for: .normal)
+            myAppLabel.isHidden = true
+        }
+    }
 }
 
